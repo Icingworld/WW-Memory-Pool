f5a89f058748708d297dd36d91028d1bbebf1fd2
icingworld <984442022@qq.com>
2025-04-29 10:12:01 +0800

    test: remove UB test

--- test/threadcache_test.cpp
+++ test/threadcache_test.cpp
@@ -27,18 +27,10 @@ TEST_F(ThreadCacheTest, SingleThreadAllocateAndDeallocate)
     // 释放内存
     thread_cache.deallocate(ptr, 8);
 
-    // 申请一个12字节的内存
-    // 实际上会对齐为16字节
-    ptr = thread_cache.allocate(12);    // 换64字节可以运行
+    ptr = thread_cache.allocate(sizeof(LargeObject));
     EXPECT_NE(ptr, nullptr);
-    // 尝试构造一个大对象
     LargeObject * obj = new(ptr) LargeObject;
     EXPECT_EQ(obj->d, std::string("hello, world! hello, world!"));
     obj->~LargeObject();
-
-    // 这里会出现UB，因为内存实际上不够构造这个对象，破坏了内存，如果申请足够的内存如64字节，则能顺利运行测试
-    EXPECT_DEATH({
-    	// 释放内存
-    	thread_cache.deallocate(ptr, 12);
-    }, ".*");
+    thread_cache.deallocate(ptr, sizeof(LargeObject));
 }
