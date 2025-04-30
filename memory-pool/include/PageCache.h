55f3da66ef753b393b3e7fc76021abf48a52cfe0
Icingworld <984442022@qq.com>
2025-05-01 01:05:10 +0800

    refactor: rewrite it with new freelist and spanlist
    - Code review
    - Todo: release memory when pagecache is destructed

--- memory-pool/include/PageCache.h
+++ memory-pool/include/PageCache.h
@@ -8,7 +8,6 @@
 namespace WW
 {
 
-constexpr std::size_t PAGE_SIZE = 4096;         // 单页大小
 constexpr std::uint8_t MAX_PAGE_COUNT = 128;    // 最大页数
 
 /**
@@ -17,16 +16,13 @@ constexpr std::uint8_t MAX_PAGE_COUNT = 128;    // 最大页数
 class PageCache
 {
 public:
-    using size_type = std::size_t;
     using page_id = std::size_t;            // 页号类型
-    using block_type = std::uint16_t;       // 内存块数量类型
+    using block_count = std::uint16_t;      // 内存块数量类型
     using page_count = std::uint8_t;        // 页数类型
-    using pointer = SpanList::pointer;
-    using void_pointer = void *;
 
 private:
     std::array<SpanList, MAX_PAGE_COUNT> _Spans;        // 页段链表数组
-    std::unordered_map<page_id, pointer> _Span_map;     // 页号到页段的映射
+    std::unordered_map<page_id, Span *> _Span_map;      // 页号到页段指针的映射
     std::recursive_mutex _Mutex;                        // 页缓存锁
 
 private:
@@ -48,25 +44,28 @@ public:
     /**
      * @brief 获取指定大小的页段
      * @param count 页数
+     * @return 成功时返回`Span *`，失败时返回`nullptr`
      */
-    pointer fetchSpan(page_count count);
+    Span * fetchSpan(page_count count);
 
     /**
      * @brief 将页段归还到页缓存
      * @param span 页段
      */
-    void returnSpan(pointer span);
+    void returnSpan(Span * span);
 
     /**
      * @brief 通过内存地址找到所属页段
+     * @param ptr 内存地址
+     * @return 成功找到返回`Span *`，失败时返回`nullptr`
      */
-    pointer FreeObjectToSpan(void_pointer ptr);
+    Span * FreeObjectToSpan(void * ptr);
 
 private:
     /**
      * @brief 从系统内存中获取指定大小的内存
      */
-    void * fetchFromSystem(block_type page_count) const noexcept;
+    void * fetchFromSystem(block_count count) const noexcept;
 };
 
 } // namespace WW
