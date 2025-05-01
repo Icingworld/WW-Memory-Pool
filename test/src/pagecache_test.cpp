a778b0b7037769b1eee475628d2cc609d4c55187
Icingworld <984442022@qq.com>
2025-05-02 00:44:17 +0800

    test: correct test

--- test/src/pagecache_test.cpp
+++ test/src/pagecache_test.cpp
@@ -13,35 +13,35 @@ TEST_F(PageCacheTest, SingleThreadFetchAndReturn)
     // 此时页缓存中应当有一个124页的页段
     WW::Span * span_4 = page_cache.fetchSpan(4);
     // 模拟切分页段为内存块
-    span_4->used = 64;
+    span_4->setUsed(64);
     EXPECT_NE(span_4, nullptr);
-    EXPECT_EQ(span_4->page_count, 4);
+    EXPECT_EQ(span_4->count(), 4);
 
     // 申请一个24页的页段
     // 此时页缓存中应当有一个100页的页段
     WW::Span * span_24 = page_cache.fetchSpan(24);
     // 模拟切分页段为内存块
-    span_24->used = 64;
+    span_24->setUsed(64);
     EXPECT_NE(span_24, nullptr);
-    EXPECT_EQ(span_24->page_count, 24);
+    EXPECT_EQ(span_24->count(), 24);
 
     // 申请一个50页的页段
     // 此时页缓存中应当有一个50页的页段
     WW::Span * span_50 = page_cache.fetchSpan(50);
     // 模拟切分页段为内存块
-    span_50->used = 64;
+    span_50->setUsed(64);
     EXPECT_NE(span_50, nullptr);
-    EXPECT_EQ(span_50->page_count, 50);
+    EXPECT_EQ(span_50->count(), 50);
 
     // 归还一个24页的页段后，页缓存中应当有一个24页的页段和一个50页的页段
-    span_24->used = 0;
+    span_24->setUsed(0);
     page_cache.returnSpan(span_24);
 
     // 归还一个4页的页段后，页缓存中应当有一个28页的页段和一个50页的页段
-    span_4->used = 0;
+    span_4->setUsed(0);
     page_cache.returnSpan(span_4);
 
     // 归还一个50页的页段后，页缓存中应当有一个128页的页段
-    span_50->used = 0;
+    span_50->setUsed(0);
     page_cache.returnSpan(span_50);
 }
