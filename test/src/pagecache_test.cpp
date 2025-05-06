a1e57b06597361a44d50d8b0aa00271e15a374f5
Icingworld <984442022@qq.com>
2025-05-07 01:14:30 +0800

    test: add multi-thread test for pagecache

--- test/src/pagecache_test.cpp
+++ test/src/pagecache_test.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include <gtest/gtest.h>
 #include <PageCache.h>
 
@@ -44,4 +46,37 @@ TEST_F(PageCacheTest, SingleThreadFetchAndReturn)
     // 归还一个50页的页段后，页缓存中应当有一个128页的页段
     span_50->setUsed(0);
     page_cache.returnSpan(span_50);
+}
+
+TEST_F(PageCacheTest, MultiThreadFetchAndReturn)
+{
+    constexpr int THREAD_NUM = 4;
+    constexpr int COUNT = 1000;
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        threads.emplace_back([i]() {
+            // 获取页缓存实例
+            WW::PageCache & page_cache = WW::PageCache::getPageCache();
+
+            std::vector<WW::Span *> spans;
+
+            for (int j = 0; j < COUNT; ++j) {
+                WW::Span * span = page_cache.fetchSpan(16 + i * 16);
+                EXPECT_NE(span, nullptr);
+                EXPECT_EQ(span->count(), 16 + i * 16);
+                span->setUsed(1);
+                spans.emplace_back(span);
+            }
+
+            for (int j = 0; j < COUNT; ++j) {
+                spans[j]->setUsed(0);
+                page_cache.returnSpan(spans[j]);
+            }
+        });
+    }
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        threads[i].join();
+    }
 }
