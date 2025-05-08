66305daa4e15c9f9e7608b09a806850da54148be
Icingworld <984442022@qq.com>
2025-05-09 00:50:43 +0800

    test: create multi-thread test for centralcache

--- test/src/centralcache_test.cpp
+++ test/src/centralcache_test.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include <gtest/gtest.h>
 #include <CentralCache.h>
 
@@ -53,4 +55,34 @@ TEST_F(CentralCacheTest, SingleThreadFetchAndReturn)
     // 归还12个16字节内存块
     // 此时页段完整，应当被直接还给页缓存
     central_cache.returnRange(16, free_object_20);
+}
+
+TEST_F(CentralCacheTest, MultiThreadFetchAndReturn)
+{
+    constexpr int THREAD_NUM = 4;
+    constexpr int COUNT = 1000;
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        threads.emplace_back([i, this]() {
+            std::vector<WW::FreeObject *> free_objects;
+
+            for (int j = 0; j < COUNT; ++j) {
+                // 申请8字节空闲内存块512个
+                // 此时刚好用一页来切分，获得512个内存块
+                WW::FreeObject * free_object = central_cache.fetchRange(8 * i + 8, WW::MAX_BLOCK_NUM);
+                EXPECT_NE(free_object, nullptr);
+                free_objects.emplace_back(free_object);
+            }
+
+            for (int j = 0; j < COUNT; ++j) {
+                central_cache.returnRange(8 * i + 8, free_objects[j]);
+            }
+        });
+    }
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        threads[i].join();
+    }
 }
